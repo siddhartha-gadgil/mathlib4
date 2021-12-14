b9e62bd220b363115bbe9ba5480e4e9b6fd25402
Floris van Doorn <fpvdoorn@gmail.com>
2021-12-14 18:24:48 +0100

    fix compiled version

--- lakefile.lean
+++ lakefile.lean
@@ -1,35 +1,8 @@
 import Lake
 
-open System Lake DSL
-
--- def cDir : FilePath := "c"
--- def ffiSrc := cDir / "ffi.cpp"
--- def buildDir := defaultBuildDir
-
--- def ffiOTarget (pkgDir : FilePath) : FileTarget :=
---   let oFile := pkgDir / buildDir / cDir / "ffi.o"
---   let srcTarget := inputFileTarget <| pkgDir / ffiSrc
---   fileTargetWithDep oFile srcTarget fun srcFile => do
---     compileO oFile srcFile #["-I", (← getLeanIncludeDir).toString] "c++"
-
--- def cLibTarget (pkgDir : FilePath) : FileTarget :=
---   let libFile := pkgDir / buildDir / cDir / "libffi.a"
---   staticLibTarget libFile #[ffiOTarget pkgDir]
-
--- package mathlib where
---   -- As mathlib does not produce an executable,
---   -- we set the default "facet" to `oleans`,
---   -- so that we can use `lake build`.
---   -- defaultFacet := PackageFacet.oleans
---   libRoots := #[`Ffi]
---   -- specify the lib as an additional target
---   moreLibTargets := #[cLibTarget "."]
+open Lake DSL
 
 package mathlib {
-  -- customize layout
   binRoot := `Mathlib.Graph
-  -- srcDir := "Mathlib"
-  -- libRoots := #[`mathlib]
-  -- specify the lib as an additional target
-  -- moreLibTargets := #[cLibTarget pkgDir]
+  supportInterpreter := true
 }

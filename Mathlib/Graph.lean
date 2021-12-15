2c93f7413c9a41a99aad87ca1255dc4cf6175c73
Floris van Doorn <fpvdoorn@gmail.com>
2021-12-15 16:13:01 +0100

    update

--- Mathlib/Graph.lean
+++ Mathlib/Graph.lean
@@ -12,6 +12,28 @@ def mapToRBMap (m : SMap α β) {cmp : α → α → Ordering} (f : β → γ) :
 
 end SMap
 
+
+protected def List.toStringSepAux {α : Type u} [ToString α] (sep : String := ", ") :
+  Bool → List α → String
+  | b,     []    => ""
+  | true,  x::xs => toString x ++ List.toStringSepAux sep false xs
+  | false, x::xs => sep ++ toString x ++ List.toStringSepAux sep false xs
+
+protected def List.toStringSep {α : Type u} [ToString α] (sep : String := ", ") : List α → String
+  | []    => "[]"
+  | x::xs => "[" ++ List.toStringSepAux sep true (x::xs) ++ "]"
+
+protected def Array.toString {α : Type u} [ToString α] (sep : String := ", ") (xs : Array α) :
+  String :=
+"#[" ++ (if xs.size == 0 then "" else
+  ((xs.get? 0).map toString).get! ++ xs.foldl (λ str x => str ++ sep ++ toString x) "" 1) ++ "]"
+
+/-- We modify the `ToString (Array α)` instance, because (1) we want a newline between items and (2)
+  we got a stack overflow when printing a very large array (>100k entries). -/
+instance [ToString α] : ToString (Array α) where
+  toString := Array.toString ",\n"
+
+
 -- namespace Lean.ConstMap
 -- def toNameSet (m : ConstMap) : NameSet :=
 --   SMap.mapToRBMap m λ _ => ()
@@ -88,7 +110,7 @@ def positions {α : Type _} (a : Array α) (cmp : α → α → Ordering) : RBMa
 (a.zip $ range a.size).foldl (λ t (x, n) => t.insert x n) ∅
 
 def sum (a : Array Float) : Float :=
-a.foldr (·+·) 0
+a.foldl (·+·) 0
 
 def take {α : Type _} [Inhabited α] (a : Array α) (n : ℕ) : Array α :=
 if a.size ≤ n then a else (range n).map (a.get! ·)
@@ -112,7 +134,7 @@ info.typeRefs.union info.valueRefs
 end Lean.ConstantInfo
 
 def isInteresting (nm : Name) (info : ConstantInfo) : Bool :=
-!(nm.isInternal || info.isUnsafe)
+!(nm.isInternal || info.isUnsafe || (`Lean).isPrefixOf info.name)
 
 /- by Gabriel -/
 def getDeclsInCurrModule : CoreM (Array Name) := do
@@ -124,13 +146,13 @@ def getAllDecls : CoreM (Array Name) := do
     (← getEnv).constants.map₁.toArray.map (·.1)
 
 /- by Gabriel -/
-def getDeclsInMathlib : CoreM (Array Name) := do
-  let mut decls ← getDeclsInCurrModule
-  let mathlibModules := (← getEnv).header.moduleNames.map ((`Mathlib).isPrefixOf ·)
-  for (declName, moduleIdx) in (← getEnv).const2ModIdx.toArray do
-    if mathlibModules[moduleIdx] then
-      decls := decls.push declName
-  decls
+-- def getDeclsInMathlib : CoreM (Array Name) := do
+--   let mut decls ← getDeclsInCurrModule
+--   let mathlibModules := (← getEnv).header.moduleNames.map ((`Mathlib).isPrefixOf ·)
+--   for (declName, moduleIdx) in (← getEnv).const2ModIdx.toArray do
+--     if mathlibModules[moduleIdx] then
+--       decls := decls.push declName
+--   decls
 
 def interestingDecls : CoreM (Array Name) := do
   let env ← getEnv
@@ -149,25 +171,12 @@ structure NameNode where
   name : Name
   outEdges : Array ℕ
   inEdges : Array ℕ
-  weight : Float
+  weight : Float -- we choose the weights so that the sum of the weights is 1
 deriving Inhabited
 
 instance : ToString NameNode :=
 ⟨λ nn => "⟨" ++ toString nn.name ++ ", using " ++ toString nn.outEdges.size ++ ", used by " ++ toString nn.inEdges.size ++
   ", " ++ toString nn.weight ++ "⟩"⟩
-
-protected def List.toStringSepAux {α : Type u} [ToString α] (sep : String := ", ") :
-  Bool → List α → String
-  | b,     []    => ""
-  | true,  x::xs => toString x ++ List.toStringSepAux sep false xs
-  | false, x::xs => sep ++ toString x ++ List.toStringSepAux sep false xs
-
-protected def List.toStringSep {α : Type u} [ToString α] (sep : String := ", ") : List α → String
-  | []    => "[]"
-  | x::xs => "[" ++ List.toStringSepAux sep true (x::xs) ++ "]"
-
-instance [ToString α] : ToString (Array α) where
-  toString a := "!#" ++ a.toList.toStringSep ",\n"
 
 /- TODO: currently we ignore all non-interesting declarations occurring in interesting declarations.
 For _proof_i and _eqn_i declarations, it would be better to point at the corresponding interesting decl -/
@@ -191,13 +200,11 @@ nn.inEdges.map λ n => g[n].name
 def outEdgeNames (g : NameGraph) (nn : NameNode) : Array Name :=
 nn.outEdges.map λ n => g[n].name
 
--- def nodesWithNoOutEdges (g : NameGraph) : List ℕ :=
-
 def weightWithNoOutEdges (g : NameGraph) : Float :=
-g.foldr (λ nn s => if nn.outEdges.isEmpty then s + nn.weight else s) 0
+g.foldl (λ s nn => if nn.outEdges.isEmpty then s + nn.weight else s) 0
 
 def totalWeight (g : NameGraph) : Float :=
-g.foldr (λ nn s => s + nn.weight) 0
+g.foldl (λ s nn => s + nn.weight) 0
 
 /--
 Every step, `w(A)`, the weight of node `A` is changed to
@@ -212,7 +219,7 @@ def step (g : NameGraph) (dampingFactor : Float := 0.85) : NameGraph :=
 let w := weightWithNoOutEdges g
 g.map λ nn => { nn with weight :=
   (1 - dampingFactor + w * dampingFactor) / Float.ofNat g.size +
-  dampingFactor * (nn.inEdges.foldr (λ n s => s + g[n].weight / Float.ofNat g[n].outEdges.size) 0) }
+  dampingFactor * (nn.inEdges.foldl (λ s n => s + g[n].weight / Float.ofNat g[n].outEdges.size) 0) }
 
 def sortByName (g : NameGraph) : NameGraph :=
 g.qsort λ nn1 nn2 => nn1.name.quickCmp nn2.name == Ordering.lt
@@ -220,60 +227,72 @@ g.qsort λ nn1 nn2 => nn1.name.quickCmp nn2.name == Ordering.lt
 def sortRevByWeight (g : NameGraph) : NameGraph :=
 g.qsort λ nn1 nn2 => nn1.weight > nn2.weight
 
-/-
-graph currently has 27035 nodes
-
-after 1 step, top weights:
-#[Nat, Eq, Lean.Name, Bool, Array, Lean.Expr, OfNat.ofNat, String, Option, instOfNatNat]
-Nat's weight is 0.029
-
--/
-
-def test (printNum : ℕ := 10) (iter : ℕ := 5) (sort? : Bool := true) : CoreM Unit := do
+def printData (printNum : ℕ := 0) (iter : ℕ := 5) (sort? : Bool := true) (trace? : Bool := false)
+  (filter : Option String := none) :
+  CoreM Unit := do
   let env ← getEnv
+  if trace? then IO.println "generating graph..."
   let g ← mkNameGraph
   if iter > 0 then
+    if trace? then IO.println "calculating weights..."
     let g := iter.iterate step g
     if sort? then
-      let g := sortRevByWeight g
-      -- IO.println $ (g.take printNum).map (·.name)
-      IO.println $ "After iterating " ++ toString iter ++ " times, the " ++ toString printNum ++
-        " entries with the highest weight:"
-      IO.println $ g.take printNum
+      if trace? then IO.println "sorting graph..."
+      let g := sortRevByWeight g -- Todo: the inEdges and outEdges point at wrong things after sorting
+      let g ←
+        if filter == some "structure" then
+          if trace? then IO.println "filtering nodes..."
+          pure $ g.filter (isStructure env ·.name)
+        else
+          pure g
+      let itemStr := if filter == some "structure" then "structures" else "entries"
+      IO.println $ "After iterating " ++ toString iter ++ " times, " ++
+        (if printNum > 0 then
+          "the " ++ toString printNum ++ " " ++ itemStr ++ " with the highest weight:"
+        else
+          "all " ++ itemStr ++ " sorted by weight")
+      IO.println $ if printNum > 0 then g.take printNum else g
     else
-      IO.println $ "After iterating " ++ toString iter ++ " times, the same entries:"
-      IO.println $ g.take printNum
+      IO.println $ "After iterating " ++ toString iter ++ " times, " ++
+        (if printNum > 0 then toString printNum else "all") ++ " entries:"
+      IO.println $ if printNum > 0 then g.take printNum else g
   else
-    IO.println $ toString printNum ++ " entries from the graph:"
-    IO.println $ g.take printNum
-
-  -- let g := step g
-  -- let nr := 22313
-  -- IO.println $ g[nr]
-  -- -- IO.println $ (g[nr].weight * (Float.ofNat g.size))
-  -- -- IO.println $ (g[nr].name)
-  -- -- IO.println $ (env.find! g[nr].name).anyRefs.toList
-  -- -- IO.println $ (outEdgeNames g g[nr])
-  -- -- IO.println $ (inEdgeNames g g[nr])
-  -- let nr := 20827
-  -- IO.println $ (g[nr])
-  -- let nr := 12341
-  -- IO.println $ (g[nr])
-  -- IO.println $ (g[nr].weight * (Float.ofNat g.size))
+    IO.println $ (if printNum > 0 then toString printNum else "all") ++ " entries from the graph:"
+    IO.println $ if printNum > 0 then g.take printNum else g
   return ()
--- set_option profiler true
--- #print importModules
--- #exit
--- #eval test 1000 10
--- #eval 1
--- #print UInt64.ofNatCore
 
--- #print IO.Error
+/--
+To compile:
+* Make sure you have `elan` (which you have if you have ever installed Lean)
+* Run `elan self update`
+* Clone/Download the `leanprover-community/mathlib4` repository
+* Checkout the branch `fpvandoorn/graph`
+* Run `lake build`
+To run, you can run something like :
+`time ./build/bin/mathlib 0 100 1 1 structure Mathbin > out.txt`
+All arguments are optional, but you have to provide the arguments in order:
+* The first argument is the number of entries printed (default = 0, which means print everything)
+* The second argument is the number of iterations of the pagerank algorithm
+* The third argument determines whether to sort the data (0 = don't sort, 1 (default) = sort)
+* The fourth argument determines whether to print some debugging info (0 (default) = no, 1 = yes)
+* The fifth argument determines which nodes to filter (currently "structure" means only print
+  structures, any other argument means no filtering)
+* The sixth argument determines which library to use ("Mathbin" (capitalized!) means use the
+  libraries of Lean 3 + Lean 4, anything else means only use the Lean 4 library).
+-/
 def main (strs : List String) : IO UInt32 := do
-  initSearchPath (← Lean.findSysroot?) ["build/lib"]
-  let env ← importModules [{module := `Mathlib}] {}
-  let args := (strs.map String.toNat!).toArray
-  -- IO.println args
-  let u ← CoreM.toIO (test (args.getD 0 10) (args.getD 1 5) (args.getD 2 0 == 0)) {} {env := env}
-  -- let u ← CoreM.toIO test {} {env := env}
+  let env ←
+    if strs.get? 5 == some "Mathbin" then
+      initSearchPath (← Lean.findSysroot?) ["../mathlib3port/build/lib",
+      "../mathlib3port/lean_packages/lean3port/build/lib/",
+      "../mathlib3port/lean_packages/mathlib/build/lib/"]
+      importModules [{module := `Mathbin}] {}
+    else
+      initSearchPath (← Lean.findSysroot?) ["build/lib"]
+      importModules [{module := `Mathlib}] {}
+  let args := ((strs.take 4).map String.toNat!).toArray
+  let u ← CoreM.toIO
+    (printData (args.getD 0 10) (args.getD 1 5) (args.getD 2 1 != 0) (args.getD 3 0 != 0)
+      (strs.get? 4))
+    {} {env := env}
   return 0
